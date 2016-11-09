81b0fd0cd74558b20b5d24816bf5a0998c697caa
DCSaunders <ds636@cam.ac.uk>
2016-11-09 13:17:00 +0000

    tweaks to tensorflow translate demo to give basic autoencoder

---
+++ seq2seq/autoencoder.py
@@ -0,0 +1,241 @@
+#!/usr/bin/env python
+"""Script for training an autoencoder model and decoding from them.
+Running this program without --decode will look for training/dev data 
+in --train_path and --dev_path and tokenize it in a very basic way,
+and then start training a model saving checkpoints to --chkpt_dir.
+Running with --decode starts an interactive loop so you can see how
+the current checkpoint autoencodes sentences
+
+Based off the Tensorflow seq2seq tutorial
+"""
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import math
+import os
+import random
+import sys
+import time
+import logging
+
+import numpy as np
+from six.moves import xrange  # pylint: disable=redefined-builtin
+import tensorflow as tf
+
+import data_utils
+import seq2seq_model
+
+tf.app.flags.DEFINE_float("learning_rate", 0.5, "Learning rate.")
+tf.app.flags.DEFINE_float("learning_rate_decay_factor", 0.99,
+                          "Learning rate decays by this much.")
+tf.app.flags.DEFINE_float("max_gradient_norm", 5.0,
+                          "Clip gradients to this norm.")
+tf.app.flags.DEFINE_integer("batch_size", 64,
+                            "Batch size to use during training.")
+tf.app.flags.DEFINE_integer("size", 512, "Size of each model layer.")
+tf.app.flags.DEFINE_integer("num_layers", 1, "Number of layers in the model.")
+tf.app.flags.DEFINE_integer("vocab_size", 40000, "Sequence vocabulary size.")
+tf.app.flags.DEFINE_string("train_path", "../simple-examples/data/ptb.train.txt", "Training data file")
+tf.app.flags.DEFINE_string("dev_path", "../simple-examples/data/ptb.valid.txt", "Development data file")
+tf.app.flags.DEFINE_string("chkpt_dir", "/tmp", "Training directory.")
+tf.app.flags.DEFINE_integer("max_train_data_size", 0,
+                            "Limit on the size of training data (0: no limit).")
+tf.app.flags.DEFINE_integer("steps_per_checkpoint", 200,
+                            "How many training steps to do per checkpoint.")
+tf.app.flags.DEFINE_boolean("decode", False,
+                            "Set to True for interactive decoding.")
+tf.app.flags.DEFINE_boolean("use_fp16", False,
+                            "Train using fp16 instead of fp32.")
+
+FLAGS = tf.app.flags.FLAGS
+
+# We use a number of buckets and pad to the closest one for efficiency.
+# See seq2seq_model.Seq2SeqModel for details of how they work.
+_buckets = [(5, 5), (10, 10), (20, 20), (40, 40)]
+
+
+def read_data(data_path, max_size=None):
+    """Read data from file and put into buckets.
+    Args:
+    data_path: path to the files with token-ids
+    it must be aligned with the source file: n-th line contains the desired
+    output for n-th line from the data_path.
+    max_size: maximum number of lines to read, all other will be ignored;
+    if 0 or None, data files will be read completely (no limit).
+    Returns:
+    data_set: a list of length len(_buckets); data_set[n] contains a list of
+    (data, data) pairs read from the provided data files that fit
+    into the n-th bucket, i.e., such that len(data) < _buckets[n]
+    data is a list of token-ids.
+    """
+    data_set = [[] for _ in _buckets]
+    with tf.gfile.GFile(data_path, mode="r") as data_file:
+        data = data_file.readline()
+        counter = 0
+        while data and (not max_size or counter < max_size):
+            counter += 1
+            if counter % 100000 == 0:
+                print("  reading data line %d" % counter)
+                sys.stdout.flush()
+            data_ids = [int(x) for x in data.split()]
+            data_ids.append(data_utils.EOS_ID)
+            for bucket_id, (bucket_size, bucket_size) in enumerate(_buckets):
+                if len(data_ids) < bucket_size:
+                    data_set[bucket_id].append([data_ids, data_ids])
+                    break
+            data = data_file.readline()
+    return data_set
+
+
+def create_model(session, forward_only):
+    """Create translation model and initialize or load parameters in session."""
+    dtype = tf.float16 if FLAGS.use_fp16 else tf.float32
+    model = seq2seq_model.Seq2SeqModel(
+        FLAGS.vocab_size,
+        FLAGS.vocab_size,
+        _buckets,
+        FLAGS.size,
+        FLAGS.num_layers,
+        FLAGS.max_gradient_norm,
+        FLAGS.batch_size,
+        FLAGS.learning_rate,
+        FLAGS.learning_rate_decay_factor,
+        forward_only=forward_only,
+        dtype=dtype)
+    ckpt = tf.train.get_checkpoint_state(FLAGS.chkpt_dir)
+    if ckpt and tf.gfile.Exists(ckpt.model_checkpoint_path):
+        print("Reading model parameters from %s" % ckpt.model_checkpoint_path)
+        model.saver.restore(session, ckpt.model_checkpoint_path)
+    else:
+        print("Created model with fresh parameters.")
+        session.run(tf.initialize_all_variables())
+    return model
+
+
+def train():
+    """Train an autoencoder on sequence data"""
+    print("Retrieving data from {} (training) and {} (dev)".format(FLAGS.train_path, FLAGS.dev_path))
+    data_train, data_dev, _ = data_utils.prepare_data(
+        FLAGS.train_path, FLAGS.dev_path, FLAGS.vocab_size)
+
+    with tf.Session() as sess:
+        # Create model.
+        print("Creating %d layers of %d units." % (FLAGS.num_layers, FLAGS.size))
+        model = create_model(sess, False)
+
+        # Read data into buckets and compute their sizes.
+        print ("Reading development and training data (limit: %d)."
+               % FLAGS.max_train_data_size)
+        dev_set = read_data(data_dev)
+        train_set = read_data(data_train, FLAGS.max_train_data_size)
+        train_bucket_sizes = [len(train_set[b]) for b in xrange(len(_buckets))]
+        train_total_size = float(sum(train_bucket_sizes))
+
+        # A bucket scale is a list of increasing  weights 0-1 that we'll use
+        # to select a bucket. [scale[i], scale[i+1]] is proportional to
+        # the size of the ith training bucket, as used later.
+        train_buckets_scale = [sum(train_bucket_sizes[:i + 1]) 
+                               / train_total_size 
+                               for i in xrange(len(train_bucket_sizes))]
+        # This is the training loop.
+        step_time, loss = 0.0, 0.0
+        current_step = 0
+        previous_losses = []
+        while True:
+            # Choose a bucket according to data distribution. We pick a random number
+            # in [0, 1] and use the corresponding interval in train_buckets_scale.
+            random_number_01 = np.random.random_sample()
+            bucket_id = min([i for i in xrange(len(train_buckets_scale))
+                             if train_buckets_scale[i] > random_number_01])
+
+            # Get a batch and make a step.
+            start_time = time.time()
+            encoder_inputs, decoder_inputs, target_weights = model.get_batch(
+                train_set, bucket_id)
+            _, step_loss, _ = model.step(sess, encoder_inputs, decoder_inputs,
+                                         target_weights, bucket_id, False)
+            step_time += (time.time() - start_time) / FLAGS.steps_per_checkpoint
+            loss += step_loss / FLAGS.steps_per_checkpoint
+            current_step += 1
+
+            # Once in a while, we save checkpoint, print statistics, and run evals.
+            if current_step % FLAGS.steps_per_checkpoint == 0:
+                # Print statistics for the previous epoch.
+                perplexity = math.exp(float(loss)) if loss < 300 else float("inf")
+                print ("global step %d learning rate %.4f step-time %.2f perplexity "
+                       "%.2f" % (model.global_step.eval(), model.learning_rate.eval(),
+                           step_time, perplexity))
+            # Decrease learning rate if no improvement was seen over last 3 times.
+                if len(previous_losses) > 2 and loss > max(previous_losses[-3:]):
+                    sess.run(model.learning_rate_decay_op)
+                previous_losses.append(loss)
+                # Save checkpoint and zero timer and loss.
+                checkpoint_path = os.path.join(FLAGS.chkpt_dir, "autoenc.ckpt")
+                model.saver.save(sess, checkpoint_path, global_step=model.global_step)
+                step_time, loss = 0.0, 0.0
+                # Run evals on development set and print their perplexity.
+                for bucket_id in xrange(len(_buckets)):
+                    if len(dev_set[bucket_id]) == 0:
+                        print("  eval: empty bucket %d" % (bucket_id))
+                        continue
+                    encoder_inputs, decoder_inputs, target_weights = model.get_batch(
+                          dev_set, bucket_id)
+                    _, eval_loss, _ = model.step(sess, encoder_inputs, decoder_inputs,
+                                                 target_weights, bucket_id, True)
+                    eval_ppx = math.exp(float(eval_loss)) if eval_loss < 300 else float(
+                          "inf")
+                    print("  eval: bucket %d perplexity %.2f" % (bucket_id, eval_ppx))
+                sys.stdout.flush()
+
+
+def decode():
+    with tf.Session() as sess:
+        # Create model and load parameters.
+        model = create_model(sess, True)
+        model.batch_size = 1  # We decode one sentence at a time.
+        # Load vocabularies.
+        vocab_path = "vocab%d" % FLAGS.vocab_size
+        vocab, rev_vocab = data_utils.initialize_vocabulary(vocab_path)
+
+        # Decode from standard input.
+        sys.stdout.write("> ")
+        sys.stdout.flush()
+        sentence = sys.stdin.readline()
+        while sentence:
+            # Get token-ids for the input sentence.
+            token_ids = data_utils.sentence_to_token_ids(tf.compat.as_bytes(sentence), vocab)
+            # Which bucket does it belong to?
+            bucket_id = len(_buckets) - 1
+            for i, bucket in enumerate(_buckets):
+                if bucket[0] >= len(token_ids):
+                    bucket_id = i
+                    break
+            else:
+                logging.warning("Sentence truncated: %s", sentence) 
+
+            # Get a 1-element batch to feed the sentence to the model.
+            encoder_inputs, decoder_inputs, target_weights = model.get_batch(
+                {bucket_id: [(token_ids, [])]}, bucket_id)
+            # Get output logits for the sentence.
+            _, _, output_logits = model.step(sess, encoder_inputs, decoder_inputs, target_weights, bucket_id, True)
+            # This is a greedy decoder - outputs are just argmaxes of output_logits.
+            outputs = [int(np.argmax(logit, axis=1)) for logit in output_logits]
+            # If there is an EOS symbol in outputs, cut them at that point.
+            if data_utils.EOS_ID in outputs:
+                outputs = outputs[:outputs.index(data_utils.EOS_ID)]
+            # Print out sentence corresponding to outputs.
+            print(" ".join([tf.compat.as_str(rev_vocab[output]) for output in outputs]))
+            print("> ", end="")
+            sys.stdout.flush()
+            sentence = sys.stdin.readline()
+
+
+def main(_):
+    if FLAGS.decode:
+        decode()
+    else:
+        train()
+
+if __name__ == "__main__":
+    tf.app.run()
---
+++ seq2seq/data_utils.py
@@ -0,0 +1,201 @@
+# Copyright 2015 The TensorFlow Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+
+"""Utilities for tokenizing and constructing vocabularies."""
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import os
+import re
+import sys
+
+from six.moves import urllib
+
+from tensorflow.python.platform import gfile
+import tensorflow as tf
+
+# Special vocabulary symbols - we always put them at the start.
+_PAD = b"_PAD"
+_GO = b"_GO"
+_EOS = b"_EOS"
+_UNK = b"_UNK"
+_START_VOCAB = [_PAD, _GO, _EOS, _UNK]
+
+PAD_ID = 0
+GO_ID = 1
+EOS_ID = 2
+UNK_ID = 3
+
+# Regular expressions used to tokenize.
+_WORD_SPLIT = re.compile(b"([.,!?\"':;)(])")
+_DIGIT_RE = re.compile(br"\d")
+
+# URLs for WMT data.
+_WMT_ENFR_TRAIN_URL = "http://www.statmt.org/wmt10/training-giga-fren.tar"
+_WMT_ENFR_DEV_URL = "http://www.statmt.org/wmt15/dev-v2.tgz"
+
+def basic_tokenizer(sentence):
+    """Very basic tokenizer: split the sentence into a list of tokens."""
+    words = []
+    for space_separated_fragment in sentence.strip().split():
+        words.extend(_WORD_SPLIT.split(space_separated_fragment))
+        return [w for w in words if w]
+
+
+def create_vocab(vocab_path, data_path, max_vocab_size,
+                      tokenizer=None, normalize_digits=True):
+    """Create vocabulary file (if it does not exist yet) from data file.
+    Data file is assumed to contain one sentence per line. Each sentence is
+    tokenized and digits are normalized (if normalize_digits is set).
+    Vocabulary contains the most-frequent tokens up to max_vocab_size.
+    We write it to vocab_path in a one-token-per-line format, so that later
+    token in the first line gets id=0, second line gets id=1, and so on.
+    Args:
+    vocab_path: path where the vocabulary will be created.
+    data_path: data file that will be used to create vocabulary.
+    max_vocab_size: limit on the size of the created vocabulary.
+    tokenizer: a function to use to tokenize each data sentence;
+    if None, basic_tokenizer will be used.
+    normalize_digits: Boolean; if true, all digits are replaced by 0s.
+    """
+    if not gfile.Exists(vocab_path):
+        print("Creating vocab %s from data %s" % (vocab_path, data_path))
+        vocab = {}
+        with gfile.GFile(data_path, mode="rb") as f:
+            counter = 0
+            for line in f:
+                counter += 1
+                if counter % 100000 == 0:
+                    print("  processing line %d" % counter)
+                line = tf.compat.as_bytes(line)
+                tokens = tokenizer(line) if tokenizer else basic_tokenizer(line)
+                for w in tokens:
+                    word = _DIGIT_RE.sub(b"0", w) if normalize_digits else w
+                    if word in vocab:
+                        vocab[word] += 1
+                    else:
+                        vocab[word] = 1
+            vocab_list = _START_VOCAB + sorted(vocab, key=vocab.get, reverse=True)
+            if len(vocab_list) > max_vocab_size:
+                vocab_list = vocab_list[:max_vocab_size]
+            with gfile.GFile(vocab_path, mode="wb") as vocab_file:
+                for w in vocab_list:
+                    vocab_file.write(w + b"\n")
+
+
+def initialize_vocab(vocab_path):
+    """Initialize vocabulary from file.
+    We assume the vocabulary is stored one-item-per-line, so a file:
+    dog
+    cat
+    will result in a vocabulary {"dog": 0, "cat": 1}, and this function will
+    also return the reversed-vocabulary ["dog", "cat"].
+    Args:
+    vocab_path: path to the file containing the vocabulary.
+    Returns:
+    a pair: the vocabulary (a dictionary mapping string to integers), and
+    the reversed vocabulary (a list, which reverses the vocabulary mapping).
+    Raises:
+    ValueError: if the provided vocab_path does not exist.
+    """
+    if gfile.Exists(vocab_path):
+        rev_vocab = []
+        with gfile.GFile(vocab_path, mode="rb") as f:
+            rev_vocab.extend(f.readlines())
+        rev_vocab = [line.strip() for line in rev_vocab]
+        vocab = dict([(x, y) for (y, x) in enumerate(rev_vocab)])
+        return vocab, rev_vocab
+    else:
+        raise ValueError("Vocab file %s not found.", vocab_path)
+
+
+def sentence_to_token_ids(sentence, vocab, tokenizer=None, normalize_digits=True):
+    """Convert a string to list of integers representing token-ids.
+    For example, a sentence "I have a dog" may become tokenized into
+    ["I", "have", "a", "dog"] and with vocabulary {"I": 1, "have": 2,
+    "a": 4, "dog": 7"} this function will return [1, 2, 4, 7].
+    Args:
+    sentence: the sentence in bytes format to convert to token-ids.
+    vocab: a dictionary mapping tokens to integers.
+    tokenizer: a function to use to tokenize each sentence;
+      if None, basic_tokenizer will be used.
+    normalize_digits: Boolean; if true, all digits are replaced by 0s.
+    Returns:
+    a list of integers, the token-ids for the sentence.
+    """
+    if tokenizer:
+        words = tokenizer(sentence)
+    else:
+        words = basic_tokenizer(sentence)
+    if not normalize_digits:
+        return [vocab.get(w, UNK_ID) for w in words]
+    # Normalize digits by 0 before looking words up in the vocab.
+    return [vocab.get(_DIGIT_RE.sub(b"0", w), UNK_ID) for w in words]
+
+
+def data_to_token_ids(data_path, data_ids_path, vocab_path,
+                      tokenizer=None, normalize_digits=True):
+    """Tokenize data file and turn into token-ids using given vocab file.
+    This function loads data line-by-line from data_path, calls the above
+    sentence_to_token_ids, and saves the result to target_path. See comment
+    for sentence_to_token_ids on the details of token-ids format.
+    Args:
+    data_path: path to the data file in one-sentence-per-line format.
+    target_path: path where the file with token-ids will be created.
+    vocab_path: path to the vocabulary file.
+    tokenizer: a function to use to tokenize each sentence;
+      if None, basic_tokenizer will be used.
+    normalize_digits: Boolean; if true, all digits are replaced by 0s.
+    """
+    if not gfile.Exists(data_ids_path):
+        print("Tokenizing data in %s" % data_path)
+        vocab, _ = initialize_vocab(vocab_path)
+        with gfile.GFile(data_path, mode="rb") as data_file:
+            with gfile.GFile(data_ids_path, mode="w") as tokens_file:
+                counter = 0
+                for line in data_file:
+                    counter += 1
+                    if counter % 100000 == 0:
+                        print(" tokenizing line %d" % counter)
+                    token_ids = sentence_to_token_ids(line, vocab, tokenizer,
+                                                      normalize_digits)
+                    tokens_file.write(" ".join([str(tok) for tok in token_ids]) + "\n")
+
+
+def prepare_data(train_path, dev_path, vocab_size, tokenizer=None):
+    """Get data, create vocabularies and tokenize data.
+  Args:
+    train_path: path to file containing training data
+    dev_path: path to file containing development data
+    vocab_size: size of the sequence vocabulary to create and use.
+    tokenizer: a function to use to tokenize each data sentence;
+      if None, basic_tokenizer will be used.
+  Returns:
+    A tuple of 3 elements:
+      (1) path to the token-ids for the training data-set,
+      (2) path to the token-ids for the development data-set,
+      (3) path to the vocab file,
+    """
+    # Create vocabularies of the appropriate sizes.
+    vocab_path = "vocab%d" % vocab_size
+    create_vocab(vocab_path, train_path, vocab_size, tokenizer=tokenizer)
+    # Create token ids for the training data.
+    train_ids_path = train_path + ".ids%d" % vocab_size
+    data_to_token_ids(train_path, train_ids_path, vocab_path, tokenizer)
+    # Create token ids for the development data.
+    dev_ids_path = dev_path + ".ids%d" % vocab_size
+    data_to_token_ids(dev_path, dev_ids_path, vocab_path, tokenizer)
+    return (train_ids_path, dev_ids_path, vocab_path)
