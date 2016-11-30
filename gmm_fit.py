20b9c2a4ce37eb80fc1deeabbb5816efa83ecd66
DCSaunders <ds636@cam.ac.uk>
2016-11-30 12:55:06 +0000

    sampling and saving samples

--- gmm_fit.py
+++ gmm_fit.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 from __future__ import division
 import argparse
+import collections
 import numpy as np
 import matplotlib.cm as cm
 import matplotlib.pyplot as plt
@@ -13,16 +14,19 @@ from tsne import tsne
 
 def get_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--file_in', '-i',
-                        help='File of vectors to fit')
-    parser.add_argument('--load_samples', 
-                        help='Location to load pickled samples')
+    parser.add_argument('--file_in', help='File of vectors to fit')
+    parser.add_argument('--load_input', 
+                        help='Location to load pickled input data from')
     parser.add_argument('--load_model',  
-                        help='Location to load pickled model')
-    parser.add_argument('--save_samples',
-                        help='Location to save pickled samples')
-    parser.add_argument('--save_model', 
+                        help='Location to load pickled model from')
+    parser.add_argument('--save_input',
+                        help='Location to save pickled input data')
+    parser.add_argument('--save_model',
                         help='Location to save pickled model')
+    parser.add_argument('--save_samples',
+                        help='Location to save pickled samples from model')
+    parser.add_argument('--plot', action='store_true', default=False,
+                        help='Project samples/model into two dimensions using TSNE, and save a plot')
     return parser.parse_args()
 
 def plot_samples(samples):
@@ -35,14 +39,11 @@ def plot_samples(samples):
     plt.clf()
 
 def plot_clusters(samples, proj_samples, gmix):
-    colors = cm.rainbow([i / gmix.n_components 
-                         for i in gmix.predict(samples)])
+    colors = cm.rainbow([i / gmix.n_components for i in gmix.predict(samples)])
     ax = plt.gca()
-    ax.scatter(proj_samples[:,0],
-               proj_samples[:,1], c=colors, alpha=0.8)
+    ax.scatter(proj_samples[:,0], proj_samples[:,1], c=colors, alpha=0.8)
     plt.show()
     plt.savefig("fitted_classes.png")
-
 
 def fit_samples(samples):
     '''Fit GMM to samples
@@ -62,27 +63,54 @@ def fit_samples(samples):
             print 'best model so far: n_components={}'.format(n_components)
     return best_gmm
     
-if __name__ == '__main__':
-    args = get_args()
-    if args.load_model:
+def get_samples(args):
+    proj_samples = None
+    if args.load_input:
+        with open(args.load_input, 'rb') as f:
+            samples, proj_samples = cPickle.load(f)
+            print 'Loaded pickled samples from {}'.format(args.load_input)
+    else:
+        samples = np.loadtxt(args.file_in)
+        print 'Loaded samples file from {}'.format(args.file_in)
+    if args.plot:
+        if not proj_samples:
+            proj_samples = tsne(samples, max_iter=200)
+        plot_samples(proj_samples)
+    return samples, proj_samples
+
+def get_model(args):
+    if args.load_model and not args.plot:
         with open(args.load_model, 'rb') as f:
             gmm = cPickle.load(f)
+        samples = proj_samples = None
     else:
-        if args.load_samples:
-            with open(args.load_samples, 'rb') as f:
-                samples, proj_samples = cPickle.load(f)
-            print 'Loaded pickled samples from {}'.format(args.load_samples)
-        else:
-            samples = np.loadtxt(args.file_in)
-            print 'Loaded samples file from {}'.format(args.file_in)
-            #proj_samples = tsne(samples, max_iter=200)
-        if args.save_samples:
-            with open(args.save_samples, 'wb') as f:
-                cPickle.dump([samples, proj_samples], f)
-        #plot_samples(proj_samples)
+        samples, proj_samples = get_samples(args)
         gmm = fit_samples(samples)
-    #plot_clusters(samples, proj_samples, gmm)
-    print gmm.sample()
+    return gmm, samples, proj_samples
+
+def save_model(args, gmm, train_samples, proj_samples, label_sample_dict):
+    if args.save_input:
+        with open(args.save_input, 'wb') as f:
+            cPickle.dump([train_samples, proj_samples], f)
     if args.save_model:
         with open(args.save_model, 'wb') as f:
-            cPickle.dump(gmm, f)    
+            cPickle.dump(gmm, f)
+    if args.save_samples:
+        with open(args.save_samples, 'wb') as f:
+            cPickle.dump(label_sample_dict, f)
+
+def sample_model(gmm, n_samples):
+    samples, labels = gmm.sample(n_samples)
+    label_sample_dict = collections.defaultdict(list)
+    for label, sample in zip(labels, samples):
+        label_sample_dict[label].append(sample)
+    return label_sample_dict
+
+if __name__ == '__main__':
+    args = get_args()
+    gmm, train_samples, proj_samples = get_model(args)
+    if args.plot:
+        plot_clusters(train_samples, proj_samples, gmm)
+    n_samples = 1000
+    label_sample_dict = sample_model(gmm, n_samples)
+    save_model(args, gmm, train_samples, proj_samples, label_sample_dict)
