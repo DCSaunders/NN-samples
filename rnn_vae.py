05f64ad6e7a8605fd6fcc963558eb9f6279ecf57
DCSaunders <ds636@cam.ac.uk>
2017-02-15 15:23:25 +0000

    convert to VAE on toy data

--- rnn_vae.py
+++ rnn_vae.py
@@ -1,193 +1,137 @@
-from __future__ import division
+# Basic libraries
 import numpy as np
 import tensorflow as tf
-from tensorflow.examples.tutorials.mnist import input_data
-from tensorflow.models.rnn.translate.seq2seq.wrapper_cells import BidirectionalRNNCell, BOWCell
-from tensorflow.python.ops import rnn, seq2seq
-import matplotlib.pyplot as plt
-import cPickle
-np.random.seed(0)
-tf.set_random_seed(0)
-
-class VariationalAutoencoder(object):
-    def __init__(self, dimensions, transfer_func=tf.nn.softplus, learning_rate=0.001, batch_size=100, use_lstm=True):
-        self.weights = dict()
-        self.n_input = dimensions['n_input']
-        self.n_steps = dimensions['n_steps']
-
-        self.initializer = tf.contrib.layers.xavier_initializer()
-        self.n_z = dimensions['n_z']
-        self.transfer_func = transfer_func
-        self.learning_rate = learning_rate
-        self.batch_size = batch_size
-        self.x = tf.placeholder(tf.float32, [self.batch_size, self.n_input]) 
-        self._init_cell(dimensions, use_lstm)
-        self._init_network(dimensions)
-        self._init_optimizer()
-        self.sess = tf.Session()
-        self.sess.run(tf.initialize_all_variables())
-
-    def _init_cell(self, dimensions, use_lstm):
-        self.enc_cell = tf.nn.rnn_cell.LSTMCell(
-            dimensions['n_h'], initializer=self.initializer)
-        self.dec_cell = tf.nn.rnn_cell.LSTMCell(
-            dimensions['n_h'], initializer=self.initializer)
-        if use_lstm:
-            dimensions['n_state'] = dimensions['n_h'] * 2
-            print 'Using LSTMs: doubling hidden layer size to {}'.format(
-                dimensions['n_state'])
-        else:
-            dimensions['n_state'] = dimensions['n_h']
-
-    def _init_network(self, dimensions):
-        self._init_weights('enc',
-                           n_in=dimensions['n_input'],
-                           n_out=dimensions['n_z'],
-                           state_in=dimensions['n_state'],
-                           state_out=dimensions['n_state'])
-        self._init_weights('dec',
-                           n_in=dimensions['n_z'],
-                           n_out=dimensions['n_input'],
-                           state_in=dimensions['n_state'],
-                           state_out=dimensions['n_h'])
-        self.z_mean, self.z_log_var = self._enc_network()
-        eps = tf.random_normal((self.batch_size, dimensions['n_z']),
-                               0, 1, dtype=tf.float32)
-        # reparameterization trick for encoder latent variable
-        self.z = tf.add(self.z_mean,
-                        tf.mul(tf.sqrt(tf.exp(self.z_log_var)), eps))
-        self.recon_mean = self._dec_network()
-    
-    def _init_weights(self, name, n_in, state_in, state_out, n_out):
-        self.weights[name] = dict(
-            weights=dict(
-                in_state=tf.Variable(self.initializer([n_in, state_in])),
-                out_mean=tf.Variable(self.initializer([state_out, n_out])),
-                out_log_var=tf.Variable(self.initializer([state_out, n_out]))),
-            biases=dict(
-                in_state=tf.Variable(tf.zeros([state_in], dtype=tf.float32)),
-                out_mean=tf.Variable(tf.zeros([n_out], dtype=tf.float32)),
-                out_log_var=tf.Variable(tf.zeros([n_out], dtype=tf.float32))))
-
-
-    def _rnn_in(self):
-        # Split to get a list of 'n_steps' tensors of shape (batch_size, n_input)
-        x = tf.split(value=self.x, num_split=self.batch_size, split_dim=0)
-        #x = tf.split(value=x, num_split=self.n_input, split_dim=1)
-        return x
-
-
-    def _enc_network(self):
-        # Encoder mapping inputs onto Gaussian in latent space
-        with tf.variable_scope("encoder"):
-            w, b = self.weights['enc']['weights'], self.weights['enc']['biases']
-            _, enc_state = rnn.rnn(self.enc_cell, self._rnn_in(), dtype=tf.float32)
-            z_mean = tf.add(tf.matmul(enc_state, w['out_mean']), b['out_mean'])
-            z_log_var = tf.add(tf.matmul(enc_state, w['out_log_var']), b['out_log_var'])
-            return z_mean, z_log_var
-
-    def _dec_network(self):
-        # Decoder mapping latent space onto Bernoulli in data space
-        with tf.variable_scope("decoder"):
-            w, b = self.weights['dec']['weights'], self.weights['dec']['biases']
-            initial_state = self.transfer_func(
-                tf.add(tf.matmul(self.z, w['in_state']), b['in_state']))
-
-            dec_out, _ = rnn.rnn(self.dec_cell, self._rnn_in(), initial_state=initial_state)
-            #dec_out, _ = seq2seq.rnn_decoder(self._rnn_in(), initial_state, self.cell)
-            x_recon_mean = tf.nn.sigmoid(tf.add(tf.matmul(dec_out[-1], w['out_mean']), b['out_mean']))
-            return x_recon_mean
-
-    def _init_optimizer(self):
-        # Find encoder (KL divergence) and decoder (E[P(X|z)]) loss
-        encoder_loss = -0.5 * tf.reduce_sum(1 + self.z_log_var
-                                            - tf.square(self.z_mean)
-                                            - tf.exp(self.z_log_var), 1)
-        #TODO: reshape here
-        decoder_loss = -tf.reduce_sum(
-                          self.x * tf.log(1e-10 + self.recon_mean) 
-                        + (1 - self.x) * tf.log(1e-10 + (1 - self.recon_mean)), 1)
-        self.cost = tf.reduce_mean(decoder_loss + encoder_loss)
-        self.optimizer = tf.train.AdamOptimizer(self.learning_rate).minimize(self.cost)
-
-    def partial_fit(self, X):
-       """Train model based on mini-batch of input data.
-       Return cost of mini-batch.
-       """
-       _, cost = self.sess.run((self.optimizer, self.cost), feed_dict={self.x: X})
-       return cost
-    
-    def transform(self, X):
-        """Transform data by mapping it into the latent space."""
-        # Note: This maps to mean of distribution, we could alternatively
-        # sample from Gaussian distribution
-        return self.sess.run(self.z_mean, feed_dict={self.x: X})
-    
-    def generate(self, z_mu=None):
-        """ Generate data by sampling from latent space.
-        If z_mu is not None, data for this point in latent space is
-        generated. Otherwise, z_mu is drawn from prior in latent space.        
-        """
-        if z_mu is None:
-            z_mu = np.random.normal(size=(self.batch_size, self.n_z))
-        # Note: This maps to mean of distribution, we could alternatively
-        # sample from Gaussian distribution
-        return self.sess.run(self.recon_mean, feed_dict={self.z: z_mu})
-    
-    def reconstruct(self, X):
-        """ Use VAE to reconstruct given data. """
-        return self.sess.run(self.recon_mean, feed_dict={self.x: X})
-
-
-def train(dimensions, learning_rate=0.001, batch_size=100,
-          training_epochs=10, display_step=1):
-    vae = VariationalAutoencoder(dimensions, learning_rate=learning_rate,
-                                 batch_size=batch_size)
-    for epoch in range(training_epochs):
-        avg_cost = 0.0
-        batch_count = int(n_samples / batch_size)
-        for i in range(batch_count):
-            batch_xs, _ = mnist.train.next_batch(batch_size)
-            cost = vae.partial_fit(batch_xs)
-            avg_cost += cost / n_samples * batch_size
-        if epoch % display_step == 0:
-            print "Epoch: {:04d} cost={:.9f}".format(epoch + 1, avg_cost)
-    return vae
-
-def plot_reconstruct(x_sample, x_reconstruct):
-    plt.figure(figsize=(8, 12))
-    for i in range(5):
-        plt.subplot(5, 2, 2*i + 1)
-        plt.imshow(x_sample[i].reshape(28, 28), vmin=0, vmax=1)
-        plt.title("Test input")
-        plt.colorbar()
-        plt.subplot(5, 2, 2*i + 2)
-        plt.imshow(x_reconstruct[i].reshape(28, 28), vmin=0, vmax=1)
-        plt.title("Reconstruction")
-        plt.colorbar()
-    plt.show()
-
-def plot_generate(vae, view_count=10, save=None):
-    fig, ax = plt.subplots(1, view_count, figsize=(view_count, 2))
-    fig.suptitle('Samples from VAE')
-    gen = vae.generate() 
-    for i in range(view_count):  
-        ax[i].imshow(gen[i].reshape(28, 28), vmin=0, vmax=1)
-    plt.show()
-    if save:
-        with open(save, 'wb') as f_out:
-            cPickle.dump(gen, f_out)
-
-dimensions = dict(n_h=128, # RNN cell hidden layer size
-                  n_steps=1,
-                  n_input=784, #MNIST data input (img shape: 28*28)
-                  n_z=20)  # Latent variable dimensionality
-mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+from tensorflow.python.ops import rnn
+tf.set_random_seed(1234)
 np.random.seed(1234)
-n_samples = mnist.train.num_examples
-batch_size = 120
-vae = train(dimensions, training_epochs=20, batch_size=batch_size)
-x_sample = mnist.test.next_batch(batch_size)[0]
-#x_reconstruct = vae.reconstruct(x_sample)
-#plot_reconstruct(x_sample, x_reconstruct)
-#plot_generate(vae, save='/home/mifs/ds636/exps/mnist/vae/sample_reconstructions')
+
+
+class LSTMAutoencoder(object):
+  """Basic version of LSTM-autoencoder.
+  (cf. http://arxiv.org/abs/1502.04681)
+  Usage:
+    ae = LSTMAutoencoder(n_hidden, inputs)
+    sess.run(ae.train)
+  """
+
+  def __init__(self, n_hidden, inputs, cell, learning_rate=0.001, reverse=True, 
+               decode_without_input=False, n_latent=10, transfer_func=tf.nn.softplus):
+    """
+    Args:
+      n_hidden : number of hidden elements of each LSTM unit.
+      inputs : a list of input tensors with size (batch_size x elem_num)
+      cell : an rnn cell object
+      learning_rate: learning rate for Adam optimizer
+      reverse : Option to decode in reverse order.
+      decode_without_input : Option to decode without input.
+      n_latent: number of latent states
+    """
+
+    self.batch_size = inputs[0].get_shape().as_list()[0]
+    self.elem_num = inputs[0].get_shape().as_list()[1]
+    self.n_latent = n_latent
+    self._enc_cell = cell
+    self._dec_cell = cell
+    self.initializer = tf.truncated_normal
+    self.transfer_func = transfer_func
+    self.learning_rate = learning_rate
+
+    with tf.variable_scope('encoder'):
+      z_mean_w = tf.Variable(self.initializer([self._enc_cell.state_size, self.n_latent]))
+      z_mean_b = tf.Variable(tf.zeros([self.n_latent], dtype=tf.float32))
+      z_logvar_w = tf.Variable(self.initializer([self._enc_cell.state_size, self.n_latent]))
+      z_logvar_b = tf.Variable(tf.zeros([self.n_latent], dtype=tf.float32))
+
+      _, enc_state = rnn.rnn(self._enc_cell, inputs, dtype=tf.float32)
+      z_mean = tf.add(tf.matmul(enc_state, z_mean_w), z_mean_b)
+      z_log_var = tf.add(tf.matmul(enc_state, z_logvar_w), z_logvar_b)
+      #reparameterisaton trick
+      eps = tf.random_normal((self.batch_size, self.n_latent), 0, 1, dtype=tf.float32)
+      self.z = tf.add(z_mean, tf.mul(tf.sqrt(tf.exp(z_log_var)), eps))
+      
+    with tf.variable_scope('decoder') as scope:
+      dec_in_w = tf.Variable(self.initializer([self.n_latent, self._dec_cell.state_size],
+                                              dtype=tf.float32))
+      dec_in_b = tf.Variable(tf.zeros([self._dec_cell.state_size], dtype=tf.float32))
+      dec_out_w = tf.Variable(self.initializer([n_hidden, self.elem_num], dtype=tf.float32))
+      dec_out_b = tf.Variable(tf.zeros([self.elem_num], dtype=tf.float32))
+
+      initial_dec_state = self.transfer_func(tf.add(tf.matmul(self.z, dec_in_w), dec_in_b))
+      #initial_dec_state = enc_state # vanilla autoencoder
+
+      if decode_without_input:
+        dec_inputs = [tf.zeros(tf.shape(inputs[0]), dtype=tf.float32)
+                      for _ in range(len(inputs))]
+        dec_out, dec_state = rnn.rnn(self._dec_cell, dec_inputs, 
+                                     initial_state=initial_dec_state, dtype=tf.float32)
+        """dec_out : (step_num x n_hidden)
+          dec_out_w : (n_hidden x elem_num)
+          dec_out_b : (elem_num)
+          output : (step_num x elem_num)
+          inp : (step_num x elem_num)
+        """
+        if reverse:
+          dec_out = dec_out[::-1]
+        dec_output = tf.transpose(tf.pack(dec_out), [1, 0, 2])
+        dec_out_w = tf.tile(tf.expand_dims(dec_out_w, 0), [self.batch_size, 1, 1])
+        self.output = tf.batch_matmul(dec_output, dec_out_w) + dec_out_b
+
+      else: 
+        dec_state = initial_dec_state
+        dec_input = tf.zeros(tf.shape(inputs[0]), dtype=tf.float32)
+        dec_out = []
+        for step in range(len(inputs)):
+          if step > 0: 
+            scope.reuse_variables()
+          dec_input, dec_state = self._dec_cell(dec_input, dec_state)
+          dec_input = tf.matmul(dec_input, dec_out_w) + dec_out_b
+          dec_out.append(dec_input)
+        if reverse:
+          dec_out = dec_out[::-1]
+        self.output = tf.transpose(tf.pack(dec_out), [1, 0, 2])
+
+    self.inp = tf.transpose(tf.pack(inputs), [1, 0, 2])
+    self.loss = tf.reduce_mean(tf.square(self.inp - self.output))
+    self.train = tf.train.AdamOptimizer(self.learning_rate).minimize(self.loss)
+
+batch_size = 50
+n_hidden = 30
+n_latent = 15
+step_num = 8
+elem_num = 1
+iteration = 500
+print_iter = 100
+max_val = 20
+p_input = tf.placeholder(tf.float32, [batch_size, step_num, elem_num])
+p_input_list = [tf.squeeze(t, [1]) for t in tf.split(1, step_num, p_input)]
+
+cell = tf.nn.rnn_cell.LSTMCell(n_hidden)
+ae = LSTMAutoencoder(n_hidden, p_input_list, cell=cell, decode_without_input=True, n_latent=n_latent)
+
+with tf.Session() as sess:
+  sess.run(tf.initialize_all_variables())
+
+  for i in range(iteration):
+    """Every sequence has size batch_size * step_num * elem_num 
+      Each sequence is a list of integers increasing by 1.
+      Initial value in each sequence is in range 0:max_value.
+      (ex. [8. 9. 10. 11. 12. 13. 14. 15])
+    """
+    # Set first value for each sequence in batch
+    r = np.random.randint(max_val, size=batch_size).reshape([batch_size, 1, 1])
+    r = np.tile(r, (1, step_num, elem_num))
+    # Set up linearly increasing sequence of same length
+    d = np.linspace(0, step_num, step_num, endpoint=False).reshape(
+      [1, step_num, elem_num])
+    d = np.tile(d, (batch_size, 1, 1))
+    random_sequences = r+d
+
+    sess.run(ae.train, {p_input: random_sequences})
+    if (i % print_iter == 0):
+        loss_val = sess.run(ae.loss, {p_input: random_sequences})
+        print "iter %d:" % (i+1), loss_val
+
+  inp, output =  sess.run([ae.inp, ae.output], {p_input:r+d})
+  print "train result: "
+  print "input: ", inp[0,:,:].flatten()
+  print "output: ", output[0,:,:].flatten()
